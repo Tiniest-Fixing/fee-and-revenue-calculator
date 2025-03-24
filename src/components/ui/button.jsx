bad287c53d4fa09ffe08794e9e5dde14e751b6c7
Tiniest-Fixing <tiniest-fixing8k@icloud.com>
2025-03-24 20:07:09 +0100

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -5,6 +5,8 @@ export function Button({ className = '', ...props }) {
     <button
       {...props}
       className={`bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded transition ${className}`}
-    />
+    >
+      {props.children}
+    </button>
   );
 }
